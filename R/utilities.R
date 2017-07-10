7927150b82a818a0fdee536f309de92b347b0aad
Guangchuang Yu <guangchuangyu@gmail.com>
2017-07-10 11:47:30 +0000

    version 0.0.9

--- R/utilities.R
+++ R/utilities.R
@@ -14,3 +14,40 @@ get_fun_from_pkg <- function(pkg, fun) {
     eval(parse(text = fun))
 }
 
+##' open working directory
+##'
+##' 
+##' @title o
+##' @return NULL
+##' @author Guangchuang Yu
+##' @export
+o <- function() {
+    os <- Sys.info()[1]
+    if (os == "Darwin") {
+	system("open .")
+    } else if (os == "Linux") {
+        system("xdg-open . &")
+    } else if (os == "Windows") {
+	wd <- sub("/", "\\", getwd())
+	cmd <- paste("explorer", wd)
+	suppressWarnings(shell(cmd))
+    }
+}
+
+##' read clipboard
+##'
+##' 
+##' @title read.cb
+##' @param ... parameters for read.table
+##' @return data.frame
+##' @author Guangchuang Yu
+##' @importFrom utils read.table
+##' @export
+read.cb <- function(...) {
+    os <- Sys.info()[1]
+    if (os == "Darwin") {
+        read.table(pipe("pbpaste"), ...)
+    } else {
+        read.table("clipboard", ...)
+    }
+}
